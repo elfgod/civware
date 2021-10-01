90bc2378110c9ca49c2a331d2c6ce965b1891796
elfgod <albertogx@icloud.com>
2021-09-30 19:21:20 -0500

    React Boilerplate Cleanup

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import Header from '../components/ui/Header'
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ui/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import ToolBar from '@material-ui/core/Toolbar'
+
+export default function Header(props) {
+    return (
+        <AppBar position="fixed">
+           <ToolBar>
+                CIVWARE   
+            </ToolBar> 
+        </AppBar>
+    )
+
+}
