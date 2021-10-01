16b30a3a0209a9cf498b25cb6ad9f07cd721edda
elfgod <albertogx@icloud.com>
2021-09-30 19:53:04 -0500

    AppBar Transitions

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import ToolBar from '@material-ui/core/Toolbar'
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+
+
+function ElevationScroll(props: Props) {
+    const { children } = props;
+  
+    const trigger = useScrollTrigger({
+      disableHysteresis: true,
+      threshold: 0
+    });
+  
+    return React.cloneElement(children, {
+      elevation: trigger ? 4 : 0,
+    });
+  }
 
 export default function Header(props) {
     return (
+        <ElevationScroll>
         <AppBar position="fixed">
            <ToolBar>
                 CIVWARE   
             </ToolBar> 
         </AppBar>
+        </ElevationScroll>
     )
 
 }
